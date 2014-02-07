92c1a1c24f27b1b34bb0f4e4b9c7c461c612938e
Kevin Walsh <walshkvn@gmail.com>
2014-02-07 14:40:20 +0000

    latest version

--- JavaCalculator/src/ie/ait/mase/calcproject/Calculator.java
+++ JavaCalculator/src/ie/ait/mase/calcproject/Calculator.java
@@ -4,38 +4,38 @@ import java.math.BigDecimal;
 import java.util.Queue;
 
 public class Calculator {
-	
 	public String calculate(String problem) {
-		//parse string into its component parts and return it in Reverse Polish form
+		// parse string into its component parts and return it in Reverse Polish
+		// form
 		CalculatorParser parser = new CalculatorParser();
 		Queue<String> problemRP = parser.parse(problem);
-		
+
 		// calculate the result of the problem in reverse polish notation
 		String result = calcProblemUsingReversePolish(problemRP);
-		
+
 		return result;
 	}
 
 	private String calcProblemUsingReversePolish(Queue<String> problemRP) {
 		// Create the Reverse Polish Calculator to use
 		ReversePolishParser rvCalc = new ReversePolishParser(this);
-		
+
 		// Feed the calculator the problem
 		String problemResult = rvCalc.calcProblem(problemRP);
-		
+
 		return problemResult;
 	}
-	
+
 	// Calculate an addition
 	public double add(double num1, double num2) {
 		return num1 + num2;
 	}
-	
+
 	// Calculate an subtraction
 	public double subtract(double num1, double num2) {
 		BigDecimal num1Bg = new BigDecimal(Double.toString(num1));
 		BigDecimal num2Bg = new BigDecimal(Double.toString(num2));
-		
+
 		return num1Bg.subtract(num2Bg).doubleValue();
 	}
 
@@ -46,9 +46,8 @@ public class Calculator {
 	public double divide(Double num1, Double num2) {
 		return num1 / num2;
 	}
-	
+
 	public double calcCos(Double num1) {
 		return Math.cos(num1);
 	}
-
 }
--- JavaCalculator/src/ie/ait/mase/calcproject/ReversePolishParser.java
+++ JavaCalculator/src/ie/ait/mase/calcproject/ReversePolishParser.java
@@ -4,23 +4,23 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class ReversePolishParser {
-	
+
 	private Calculator calc;
 	private Stack<String> numberStack = new Stack<String>();
-	
+
 	// Operation Constants
-	private final char PLUS = '+', SUBTRACTION = '-', MULTIPLICATION = '*', 
-			DIVISION='/'; // add more as add functionality
-	
+	private final String PLUS = "+", SUBTRACTION = "-", MULTIPLICATION = "*",
+			DIVISION = "/", POWER = "^", ROOT = "\u221A", SIN="sin"; // add more as add functionality
+
 	// Constructor
 	public ReversePolishParser(Calculator calc) {
 		this.calc = calc; // need the calculator to call the operations from.
 	}
-	
-	//Pre: The problem to solve must be in reverse polish form
-	//Post: Returns the result of the problem as a String
+
+	// Pre: The problem to solve must be in reverse polish form
+	// Post: Returns the result of the problem as a String
 	public String calcProblem(Queue<String> problemRPForm) {
-		
+
 		// Scan the given queue getting each element
 		int queueSize = problemRPForm.size();
 		for (int i = 0; i < queueSize; i++) {
@@ -30,63 +30,81 @@ public class ReversePolishParser {
 			if (!isOperator(nextToken))
 				numberStack.add(nextToken);
 			else {
-				// if operation requires two numbers:
-				// if(isFuntion())
-				String num2 = numberStack.pop();
-				String num1 = numberStack.pop();
-				// else
-				// take one number
-				
+				if(nextToken.length()>1){
+					String num1 = numberStack.pop();
+					numberStack.add(String.valueOf(calculateResult(num1,"1",
+							nextToken)));
+				}else{
+					String num2 = numberStack.pop();
+					String num1 = numberStack.pop();
+					numberStack.add(String.valueOf(calculateResult(num1, num2,
+							nextToken)));
+				}
 				// calculate result
-				numberStack.add(String.valueOf(calculateResult(num1, num2, nextToken)));
 				
+
 			}
 		}
-		
+
 		// return top of stack as result
 		return numberStack.pop();
 	}
-	
-	//Pre: 
-	//Post: Returns the result of the calculator as a double
+
+	// Pre:
+	// Post: Returns the result of the calculator as a double
 	private double calculateResult(String num1, String num2, String nextToken) {
 		double result = 0.0;
-		
-		switch (nextToken.charAt(0)) {
-		case PLUS:
+
+		switch (nextToken) {
+		case "+":
 			result = calc.add(Double.valueOf(num1), Double.valueOf(num2));
 			break;
-		case SUBTRACTION:
+		case "-":
 			result = calc.subtract(Double.valueOf(num1), Double.valueOf(num2));
 			break;
-		case MULTIPLICATION:
+		case "*":
 			result = calc.multiply(Double.valueOf(num1), Double.valueOf(num2));
 			break;
-		case DIVISION:
+		case "/":
 			result = calc.divide(Double.valueOf(num1), Double.valueOf(num2));
 			break;
+		case "^":
+			result = Math.pow(Double.valueOf(num1),Double.valueOf(num2));
+			break;
+		case "\u221A":
+			result = Math.pow(Double.valueOf(num2), 1 / Double.valueOf(num1));
+			break;
+		case "sin":
+			result = Math.sin(Double.valueOf(num1));
 		default:
-			System.out.println("This operation has not yet been implemented: " + nextToken);
+			System.out.println("This operation has not yet been implemented: "
+					+ nextToken);
 			break;
 		}
-		
+
 		return result;
 	}
 
 	private boolean isOperator(String nextToken) {
-		
+
 		boolean result = false;
-		
+
 		// Check to see if this is one of our tokens
-		switch (nextToken.charAt(0)) {
+		switch (nextToken) {
 		default:
 			result = false;
 			break;
-		case PLUS: case SUBTRACTION: case MULTIPLICATION: case DIVISION:
+		case PLUS:
+		case SUBTRACTION:
+		case MULTIPLICATION:
+		case DIVISION:
+		case POWER:
+		case ROOT:
+		case SIN:
 			result = true;
 			break;
 		}
-		
+
 		return result;
 	}
 
